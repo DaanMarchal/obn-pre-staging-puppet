d627fd2cb952e365d51c5ce174cf529fde263549
DaanMarchal <daan.marchal@gmail.com>
2022-03-24 16:21:38 +0000

    7553d332ce867d03005edbe8cd3ff48c9b8ffe1c - makefile

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ mkdir -p ${BASE_DIR}
 cd ${BASE_DIR}
 sudo wget -O obn-pre-staging-puppet.zip https://github.com/DaanMarchal/obn-pre-staging-puppet/raw/master/obn-pre-staging-puppet.zip
 unzip -o obn-pre-staging-puppet.zip
+rm obn-pre-staging-puppet.zip
 
 sudo wget https://apt.puppet.com/puppet7-release-focal.deb
 sudo dpkg -i puppet7-release-focal.deb
+rm puppet7-release-focal.deb
 
 sudo apt update
 sudo apt install -y puppet-agent
