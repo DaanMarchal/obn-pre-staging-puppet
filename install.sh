db2e2dbb64b735363a25d809652dec0edf02a15f
DaanMarchal <daan.marchal@gmail.com>
2022-03-24 13:53:05 +0000

    c45710d281ca9bad57eebb0836c00650897dea98 - raw zip link

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt install -y git-core make
 sudo apt install -y unzip
 mkdir -p ${BASE_DIR}
 cd ${BASE_DIR}
-sudo wget https://github.com/DaanMarchal/obn-pre-staging-puppet/blob/master/obn-pre-staging-puppet.zip
+sudo wget https://github.com/DaanMarchal/obn-pre-staging-puppet/raw/master/obn-pre-staging-puppet.zip
 unzip obn-pre-staging-puppet.zip
 
 sudo wget https://apt.puppet.com/puppet7-release-focal.deb
