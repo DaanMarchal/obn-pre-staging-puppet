bcb215e8d03c494995f4ee630f9f7d742e2bf477
DaanMarchal <daan.marchal@gmail.com>
2022-03-25 15:13:47 +0000

    7f0a742e3c6a429a1cf5cd0aa3c7ad33b7bc390b - python install in bash instead of puppet

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 BASE_DIR="/etc/puppet/obn-pre-staging-puppet"
 
 sudo apt update
-sudo apt install -y git-core make net-tools software-properties-common
-sudo apt install -y unzip
+sudo apt install -y git-core make net-tools software-properties-common unzip
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+
 mkdir -p ${BASE_DIR}
 cd ${BASE_DIR}
 sudo wget -O obn-pre-staging-puppet.zip https://github.com/DaanMarchal/obn-pre-staging-puppet/raw/master/obn-pre-staging-puppet.zip
@@ -17,5 +19,6 @@ rm puppet7-release-focal.deb
 
 sudo apt update
 sudo apt install -y puppet-agent
+sudo apt install -y python3.10 python3.10-venv
 
 sudo /opt/puppetlabs/bin/puppet apply --modulepath modules/ manifests/site.pp
