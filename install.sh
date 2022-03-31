b210f66af18f4c69e95c7d420e73e320c6d7483f
DaanMarchal <daan.marchal@gmail.com>
2022-03-31 10:34:17 +0200

    rteset2

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ BASE_DIR="/etc/puppet/obn-pre-staging-puppet"
 sudo apt update
 sudo apt install -y git-core make net-tools software-properties-common unzip
 
+# TODO: -y?
 sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt update
+sudo apt install -y puppet-agent
+sudo apt install -y python3.10 python3.10-venv
 
 mkdir -p ${BASE_DIR}
 cd ${BASE_DIR}
@@ -17,8 +22,16 @@ sudo wget https://apt.puppet.com/puppet7-release-focal.deb
 sudo dpkg -i puppet7-release-focal.deb
 rm puppet7-release-focal.deb
 
-sudo apt update
-sudo apt install -y puppet-agent
-sudo apt install -y python3.10 python3.10-venv
+# gather login creds
+echo "Login to obn-pre-staging application:"
+read -p 'Username: ' uservar
+echo ""
+read -sp 'Password: ' passvar
+echo ""
+# store creds in hiera
+sed -i "s,obn_pre_staging::username: \".*\",obn_pre_staging::username: \"$uservar\"", ./hieradata/common.yaml
+sed -i "s,obn_pre_staging::password: \".*\",obn_pre_staging::password: \"$passvar\"", ./hieradata/common.yaml
 
-sudo /opt/puppetlabs/bin/puppet apply --modulepath modules/ manifests/site.pp
+
+
+sudo /opt/puppetlabs/bin/puppet apply -v --debug --test --modulepath modules/ manifests/site.pp
