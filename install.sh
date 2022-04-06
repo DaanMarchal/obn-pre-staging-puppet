a8c54e8460d74def83b27ecddda3cde149bcb7fa
DaanMarchal <daan.marchal@gmail.com>
2022-04-06 14:00:12 +0000

    d11b2c5b9d905551347e9e49a91d5503555bc248 - sudo

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 
 sudo wget https://apt.puppet.com/puppet7-release-focal.deb
 sudo dpkg -i puppet7-release-focal.deb
-rm puppet7-release-focal.deb
+sudo rm puppet7-release-focal.deb
 
 sudo apt update
 sudo apt install -y puppet-agent
@@ -19,13 +19,13 @@ sudo apt install -y python3.10 python3.10-venv
 sudo mkdir -p ${BASE_DIR}
 cd ${BASE_DIR}
 sudo wget -O obn-pre-staging-puppet.zip https://github.com/DaanMarchal/obn-pre-staging-puppet/raw/master/obn-pre-staging-puppet.zip
-unzip -o obn-pre-staging-puppet.zip
-rm obn-pre-staging-puppet.zip
+sudo unzip -o obn-pre-staging-puppet.zip
+sudo obn-pre-staging-puppet.zip
 
-sudo touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
-systemctl restart NetworkManager
-sudo nmcli con add type vlan ifname vlan100 dev eth0 id 100 ip4 192.168.200.254/24
-nmcli connection up vlan-vlan100
+#sudo touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
+#sudo systemctl restart NetworkManager
+#sudo nmcli con add type vlan ifname vlan100 dev eth0 id 100 ip4 192.168.200.254/24
+#sudo nmcli connection up vlan-vlan100
 
 ./login.sh
 
